4f6cc4ccc726a74469541068639fe66f08c873d9
Gary <gary@Garys-iMac.local>
2022-07-15 11:09:21 -0400

    test

--- tests/test_users.py
+++ tests/test_users.py
@@ -1,6 +1,6 @@
 import pytest
 from .database import client, session
-
+from app import schemas
 
 @pytest.fixture()
 def test_user(client):
@@ -9,16 +9,20 @@ def test_user(client):
     res = client.post("/users", json=user_data)
 
     assert res.status_code == 201
-    return
+    new_user = res.json()
+    new_user['password'] = user_data['password']
+    return new_user
 
 def test_create_user(client):
     res = client.post("/users", json={"email":"joe@gmail.com", "password":"123"})
-    print(res.json())
+    # print(res.json())
 
     assert res.json().get("email") == "joe@gmail.com"
     assert res.status_code == 201
 
 def test_user_login(client, test_user):
-    res = client.post("/login", json={"email": "joe@gmail.com", "password": "123"})
-    print(res.json())
+    res = client.post("/login", json={"email": test_user['email'], "password": test_user['password']})
+
+    login_res = schemas.Token(**res.json())
+
     assert res.status_code == 200
